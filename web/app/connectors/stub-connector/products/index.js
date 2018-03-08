a0341e80b5db789acb9a92e7a8fb771f53f0a4de
Richard Sexton <rsexton404@gmail.com>
2018-03-08 13:18:01 -0800

    Solution for Creating a Simple PDP Part 4

--- web/app/connectors/stub-connector/products/index.js
+++ web/app/connectors/stub-connector/products/index.js
@@ -4,6 +4,9 @@
 
 /* eslint-disable no-unused-vars */
 
+import {productDetailsParser, productDetailsUIParser} from './parsers'
+import {fetchPageData} from '../app/'
+
 import {
     receiveProductDetailsProductData,
     receiveProductDetailsUIData
@@ -14,59 +17,24 @@ import {receiveFormInfo} from '../actions'
 export const initProductDetailsPage = (url, routeName) => (dispatch) => {
     console.log('[Stub Connector] Called initProductDetailsPage stub with arguments:', url, routeName)
 
-    const id = '1'
+    return dispatch(fetchPageData(url))
+    .then((res) => {
+        const [$, $response] = res
 
-    const image = {
-        src: '//via.placeholder.com/350x350',
-        alt: 'My Product'
-    }
-
-    const exampleData = {
-        [id]: {
-            price: '123.00',
-            available: true,
+        const productDetailsData = {
+            ...productDetailsParser($, $response),
             href: url,
-            thumbnail: image,
-            title: 'My Product',
-            images: [image, image],
-            id: '1',
-            description: 'This is my hard coded data.',
-            pageMeta: {
-                title: 'My Product',
-                description: 'This is my hard coded data.',
-                keywords: 'My Product'
-            }
+            variationCategories: [],
+            variants: []
         }
-    }
 
-    const exampleUIData = {
-        [id]: {
-            breadcrumbs: [{
-                href: '/',
-                text: 'Home'
-            }, {
-                href: url,
-                text: 'Product 1'
-            }],
-            itemQuantity: 1
-        }
-    }
+        const {id} = productDetailsData
 
-    const exampleFormData = {
-        [id]: {
-            submitUrl: 'submit',
-            method: 'POST',
-            uenc: '',
-            hiddenInputs: {}
-        }
-    }
-
-    // For more information on the shape of the expected data, see ../../products/types
-    dispatch(receiveCurrentProductId(id))
-    dispatch(receiveProductDetailsProductData(exampleData))
-    dispatch(receiveProductDetailsUIData(exampleUIData))
-    dispatch(receiveFormInfo(exampleFormData))
-    return Promise.resolve()
+        dispatch(receiveCurrentProductId(id))
+        dispatch(receiveProductDetailsProductData({[id]: productDetailsData}))
+        dispatch(receiveProductDetailsUIData({[id]: productDetailsUIParser($, $response)}))
+    })
+    .catch((error) => { console.info(error.message) })
 }
 
 export const getProductVariantData = (variationSelections, variants, categoryIds) => (dispatch) => {
